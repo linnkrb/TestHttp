0283c54bdcccf2440529ce85caecc4b3bd7e0882
Linn Kristin Bjørkli <linnkristin@gmail.com>
2018-02-27 10:09:29 +0100

    First commit

---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Foundation;
+using UIKit;
+
+namespace TestHttp.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;  
+     
+
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+
+
+            return base.FinishedLaunching(app, options);
+        }
+
+        private void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Console.WriteLine("Exception");
+        }
+
+        private void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine("Exception");
+        }
+    }
+}
